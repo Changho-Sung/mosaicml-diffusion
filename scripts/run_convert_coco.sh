5f51e801e6450ea16d9c3364bd62054bc9733571
Changho-Sung <changho.sung@ridi.com>
2023-08-07 11:01:47 +0900

    update

--- scripts/run_convert_coco.sh
+++ scripts/run_convert_coco.sh
@@ -1,4 +1,4 @@
 python convert_coco.py \
-  --data_path ~/datasets/coco/val2014/ \
+  --data_path ~/datasets/coco/ \
   --annotation_path ~/datasets/coco/annotations/captions_val2014.json \
   --remote s3://initial-ai-datasets/image/processed/coco-mds/

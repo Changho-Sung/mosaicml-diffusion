3e792771f5519019e62beeee4cb697b0800a2df3
Changho-Sung <changho.sung@ridi.com>
2023-08-07 09:45:05 +0900

    update

--- scripts/run_sbu_cloudwriter.sh
+++ scripts/run_sbu_cloudwriter.sh
@@ -1,9 +1,8 @@
 python sbu_cloudwriter.py \
   --path s3://initial-ai-datasets/image/downloaded/sbu-captions/ \
   --local ~/cache/sbu \
-  --remote s3://initial-ai-datasets/image/downloaded/sbu-captions-mds/ \
+  --remote s3://initial-ai-datasets/image/processed/sbu-captions-mds/ \
   --keep_parquet \
   --keep_cache \
   --bucketed \
-  --subfolder 2
-  
+  --subfolder 3
